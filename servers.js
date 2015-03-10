7a0ad1e791048e45e07a4f2e3eb98ac2f06841e5
Julien <dev.vialajulien@gmail.com>
2015-03-10 21:47:32 +0100

    Default app skeleton done. :smile:

--- servers.js
+++ servers.js
@@ -1,7 +1,35 @@
-'user strict';
+'use strict';
 
-var koa = require('koa');
-var app = koa();
+let koa = require('koa'),
+    router = require('koa-router'),
+    serve = require('koa-static'),
+    views = require('koa-views'),
+    compress = require('koa-compress'),
+    marko = require('marko');
 
+let app = koa();
+let render = views( __dirname + '/views/' );
 
-var server = app.listen(3000);
+app.use(compress({
+    flush: require('zlib').Z_SYNC_FLUSH
+}));
+
+app.use(serve( __dirname + '/public'));
+app.use(router(app));
+
+app.get('/', function *() {
+    this.body = marko.load('./views/pages/home/home.marko').renderSync({});
+    this.type = "text/html";
+});
+
+app.get('/pickup', function *() {
+    this.body = marko.load('./views/pages/pickup/pickup.marko').renderSync({});
+    this.type = "text/html";
+});
+
+app.get('/gimme', function *() {
+    this.body = marko.load('./views/pages/gimme/gimme.marko').renderSync({});
+    this.type = "text/html";
+});
+
+let server = app.listen(3000);
